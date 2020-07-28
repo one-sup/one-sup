218cf97e6e107c4f620988a7e3af6d8162c917a9
Orta <git@orta.io>
2020-07-28 19:32:24 -0400

    Adds the scheduler

---
+++ packages/funcs/scheduler/index.js
@@ -0,0 +1,10 @@
+/** @type {import("@azure/functions").AzureFunction} */
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+
+    if (myTimer.IsPastDue){
+        context.log('JavaScript is running late!');
+    }
+
+    context.log('JavaScript timer trigger function ran!', timeStamp);
+};
